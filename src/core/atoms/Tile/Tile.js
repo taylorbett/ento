331ad6911b16994ca386bf2c8ea08cade224474d
TaylorBett <taylor.bett@hotmail.com>
2018-05-20 17:18:42 +1000

    Style and logging updates for Tile and Shift components

--- src/core/atoms/Tile/Tile.js
+++ src/core/atoms/Tile/Tile.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Tile = styled.div`
-    display: {$props => $props.block ? 'block' : 'inline-block'};
+    display: ${props => props.block ? 'block' : 'inline-block'};
     border: 1px solid #e4e9ed;
     border-radius: 4px;
     background: #f8fafd;
@@ -15,6 +15,7 @@ const Tile = styled.div`
 
 export class TileComponent extends React.PureComponent {
     render() {
+        console.log('rendering tile with', this.props);
         return (
             <Tile>
                 {this.props.children}
--- src/core/molecules/Shift/Shift.js
+++ src/core/molecules/Shift/Shift.js
@@ -5,10 +5,11 @@ import PropTypes from 'prop-types';
 import Tile from '../../atoms/Tile';
 
 const Shift = styled.div`
-    width: {$props => $props.long ? '100%' : '144px'};
+    width: ${props => props.long ? '100%' : '144px'};
     height: 34px;
     padding: 8px 10px;
     cursor: pointer;
+    color: ${props => props.revealed ? '#727375' : 'inherit'};
 `;
 
 const ShiftText = styled.p`
@@ -21,11 +22,23 @@ const ShiftTime = ShiftText.extend`
     font-weight: bold;
 `;
 
-const Alert = styled.div`
+const AlertsContainer = styled.div`
+    height: 100%;
     position: absolute;
     right: 0;
+    top: 0;
     text-transform: uppercase;
+    font-weight: bold;
+    font-size: 0.65em;
     max-width: 128px;
+    margin: -1px -1px 0 0;
+`;
+
+const Alert = styled.div`
+    border-radius: 0 4px 4px 0;
+    height: 26px;
+    padding: 0 8px;
+    line-height: 26px;
 `;
 
 const CriticalAlert = Alert.extend`
@@ -35,6 +48,7 @@ const CriticalAlert = Alert.extend`
 
 const LowAlert = Alert.extend`
     background: #f6e416;
+    color: #1f1f24;
 `;
 
 // MUTED TEXT COLOUR #727375
@@ -49,19 +63,23 @@ export class ShiftComponent extends React.Component {
     }
 
     handleClick() {
+        console.log('handleClick!');
         this.setState({
             revealed: !this.state.revealed,
         });
     }
     
     render() {
+        console.log('rendering shift with', this.props)
         return (
-            <Tile onClick={() => this.handleClick()} block={this.props.long}>
-                <Shift>
+            <Tile block={this.props.long}>
+                <Shift revealed={this.state.revealed} onClick={() => this.handleClick()}>
                     <ShiftTime>{this.props.startTime} - {this.props.endTime}</ShiftTime>
                     <ShiftText>{this.props.role}</ShiftText>
-                    {this.props.alerts.critical.length ? <CriticalAlert>{this.props.alerts.critical.length} critical alerts</CriticalAlert> : null}
-                    {this.props.alerts.low.length ? <LowAlert>{this.props.alerts.low.length} low alerts</LowAlert> : null}
+                    <AlertsContainer>
+                        {this.props.alerts.critical.length ? <CriticalAlert>{this.props.alerts.critical.length} critical alerts</CriticalAlert> : null}
+                        {this.props.alerts.low.length ? <LowAlert>{this.props.alerts.low.length} low alerts</LowAlert> : null}
+                    </AlertsContainer>
                 </Shift>
             </Tile>
         );
